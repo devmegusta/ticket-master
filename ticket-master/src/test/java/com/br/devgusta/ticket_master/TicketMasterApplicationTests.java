f504155c619c6465cde1c1b87fd06132764e493a
Gustavo Silva <nomegusta12345mc@gmail.com>
2025-07-10 22:13:22 -0300

    first commit project ticket-master

---
+++ ticket-master/src/test/java/com/br/devgusta/ticket_master/TicketMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.devgusta.ticket_master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
