669e9c67c2f18cd367715562d10d6f467932fae0
devmegusta <nomegusta12345mc@gmail.com>
2025-08-06 01:29:23 -0300

    Created the HTML page for exebitions os tickets.

--- ticket-master/src/main/java/com/br/devgusta/ticket_master/controller/WEB/PageAnalystController.java
+++ ticket-master/src/main/java/com/br/devgusta/ticket_master/controller/WEB/PageAnalystController.java
@@ -1,4 +1,11 @@
 package com.br.devgusta.ticket_master.controller.WEB;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PageAnalystController {
+    @GetMapping("/analyst/home")
+    public String showAnalystPageForm(Model model){return "analyst-home";}
 }
