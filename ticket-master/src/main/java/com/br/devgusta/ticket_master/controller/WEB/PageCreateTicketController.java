6b71324691ce1a49043aa581f1cff7c9f692fc7c
devmegusta <nomegusta12345mc@gmail.com>
2025-08-11 21:52:56 -0300

    Project conclued with success!

--- ticket-master/src/main/java/com/br/devgusta/ticket_master/controller/WEB/PageCreateTicketController.java
+++ ticket-master/src/main/java/com/br/devgusta/ticket_master/controller/WEB/PageCreateTicketController.java
@@ -1,14 +1,30 @@
 package com.br.devgusta.ticket_master.controller.WEB;
+import com.br.devgusta.ticket_master.DTO.TicketRequestDTO;
+import com.br.devgusta.ticket_master.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-
 public class PageCreateTicketController {
+
+    @Autowired
+    private TicketService ticketService;
+
+
     @GetMapping("/tickets/create")
     public String showCreateTicketForm(Model model) {
         return "create-ticket";
     }
-}
 
+
+    @PostMapping("/tickets/create")
+    public String createTicket(@ModelAttribute TicketRequestDTO ticketDTO, Model model) {
+        ticketService.createTicket(ticketDTO);
+        model.addAttribute("message", "Chamado criado com sucesso! Fique atento(a) ao seu e-mail!");
+        return "create-ticket-success";
+    }
+}
---
+++ ticket-master/src/main/java/com/br/devgusta/ticket_master/controller/WEB/PageCreatedTicketForm.java
@@ -0,0 +1,8 @@
+package com.br.devgusta.ticket_master.controller.WEB;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PageCreatedTicketForm {
+
+}
